5b47663715b70c13305859fc49df1bae8f15ac34
Adityasony69 <adityasony926@gmail.com>
2026-07-26 11:52:10 +0530

    ai itegration based on machine learning is done

--- recon_ai.py
+++ recon_ai.py
@@ -11,4 +11,23 @@ print("        PROJECT RECON ")
 print("=" * 50)
 
 # Get news from user
-news = input("\n📰 Paste a news article:\n\n> ")
+news = input("\n Paste a news article:\n\n> ")
+
+#convert the nws article info TF-IDF feature 
+news_vector = vectorizer.transform([news])
+
+#predicting using the trained model
+prediction = model.predict(news_vector)
+confidence = model.predict_proba(news_vector)
+
+#converting result into redable text 
+result = 'REAL' if prediction[0]==1 else 'FAKE'
+
+#heighest confidence perfcentage 
+confidence_score = max(confidence[0])*100
+
+
+print("\n" + "=" * 50)
+print(f"Prediction : {result}")
+print(f"Confidence : {confidence_score:.2f}%")
+print("=" * 50)
